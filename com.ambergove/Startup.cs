be54a128c470be3f40954fabd81ba1e641de5cdc
Shane Warren <shanewwarren@gmail.com>
2015-03-14 09:08:34 -0700

    Backing up before I start changing things quite a bit.

---
+++ com.ambergove/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(com.ambergove.Startup))]
+namespace com.ambergove
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
