7743f5de7d63941f411d3e8d791affea8abca28b
Shane Warren <shanewwarren@gmail.com>
2015-04-29 19:39:15 -0700

    Minor update.

--- com.ambergove/App_Start/BundleConfig.cs
+++ com.ambergove/App_Start/BundleConfig.cs
@@ -9,26 +9,27 @@ namespace com.ambergove
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.sidr.min.js"));
+                        "~/js/jquery-{version}.js",
+                        "~/js/jquery.dropotron.min.js",
+                        "~/js/jquery.scrollgress.min.js",
+                        "~/js/skel.min.js",
+                        "~/js/skel-layers.min.js",
+                        "~/js/init.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/js/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/js/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/jquery.sidr.dark.css",
-                      "~/Content/site.css",
-                      "~/Content/overrides.css"));
+            bundles.Add(new StyleBundle("~/css/all").Include(
+                      "~/css/skel.css",
+                      "~/css/font-awesome.min.cs",
+                     
+                      "~/css/site.css",
+                      "~/css/overrides.css"));
         }
     }
 }
